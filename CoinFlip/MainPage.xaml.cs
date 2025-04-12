7a1345bdcbd660bcc83b26aa9f27f53b688229a5
RenatoCBarbosa <renatobarbosa279@hotmail.com>
2025-04-11 22:29:25 -0300

    PAM - CoinFlip

--- CoinFlip/MainPage.xaml.cs
+++ CoinFlip/MainPage.xaml.cs
@@ -13,7 +13,7 @@ namespace CoinFlip
             }
             else if (lado == 1)
             { 
-                //DisplayAlert("Resultado", "Você ganhou!", "Ok");
+               // DisplayAlert("Resultado", "Você ganhou!", "Ok");
             }
         }
     }
@@ -32,35 +32,38 @@ namespace CoinFlip
 
         private void FlipButton_Clicked(object sender, EventArgs e)
         {
-            Random random = new Random();
             //Algoritmo
             //Verificar qual opção está selecionada
             //Fazer o sorteio da moeda
             //Exibir o lado que foi sorteado
             //Verificar se a opção == lado escolhido
             //Exibir o resultado em um alerta
-            CoinFlip.lado = random.Next(2);
-            switch (CoinFlip.lado)
-            {
-                case 0:
-                    {
-                        ImagemMoeda.Source = "coroaa.png";
-                        CoinFlip.GirarMoeda(0);
-                    }
-                    break;
 
-                case 1:
-                    {
-                        ImagemMoeda.Source = "caraa.png";
-                        CoinFlip.GirarMoeda(1);
-                    }
-                    break;
+            Random random = new Random();
+            CoinFlip.lado = random.Next(2);
+
+
+
+            if (PickerMoeda.SelectedItem.Equals("Cara") && this.CoinFlip.lado == 0)
+            {
+                DisplayAlert("Resultado", "Você ganhou!", "Ok");
+            }
+            else if (PickerMoeda.SelectedItem.Equals("Coroa") && this.CoinFlip.lado == 1)
+            {
+                DisplayAlert("Resultado", "Você ganhou!", "Ok");
+            }
+
+            else
+            {
+                DisplayAlert("Resultado", "Você perdeu!", "Ok");
             }
 
         }
+
+        }
     }
-   
-}
+
+ 
 
 //Tirar o objeto coinflip e colocar esse código de baixo do switch case para voltar ao normal
 
